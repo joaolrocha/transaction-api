53588ae1107e8794a3a47b9abf6b4412473120d6
joaolrocha <joaolrocha07@gmail.com>
2025-07-16 18:35:38 -0300

    feat: implement Helmet.js for basic security protection

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet()); // Proteção contra vulnerabilidades comuns
 
   // Configuração do Swagger
   const config = new DocumentBuilder()
